ae5148a13a467ca0944f450228272b790b630916
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2024-12-03 12:00:35 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const initData = require("./data.js")
+const Listing = require("../models/listings.js")
+// const { init } = require("../models/listings.js")
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/aishkero'
+
+
+
+main().then(() => {
+    console.log("Connected to DB")
+})
+    .catch((err) => {
+        console.log(err)
+    })
+
+async function main() {
+    await mongoose.connect( MONGO_URL)
+}
+
+
+const initDB = async () => {
+    await Listing.deleteMany({})
+    await Listing.insertMany(initData.data)
+    console.log("Data was initialised")
+}
+initDB();
